406fa080b4b87a5be3beb87bcb32b43b5ee30b73
root <root@qiandeng-hz2.(none)>
2013-08-22 21:02:35 +0800

    init

---
+++ src/main/java/com/github/dtf/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.github.dtf.utils;
+
+public class StringUtils {
+
+	public static String stringifyException(Throwable e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
